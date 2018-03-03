c405677c06eb5e8de65eac343239fc8cc1738753
Amit Kumar <dtu.amit@gmail.com>
2018-03-03 23:00:42 +0000

    Add trigger for ballot run and add some docs

--- meetup_ballot/ballot.py
+++ meetup_ballot/ballot.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from datetime import datetime
+from datetime import timedelta
 import random
 import logging
 import os
@@ -9,14 +11,24 @@ from meetup_ballot.meetup import MeetupClient
 MEETUP_KEY_VAR = 'MEETUP_KEY'
 MEETUP_URLNAME_VAR = 'MEETUP_URLNAME'
 MAX_RSVPS_VAR = 'MAX_RSVPS'
+RSVP_BEFORE_DAYS = 7
 
 
 def setup_logging():
+    """
+    Setups logging for the ballot.
+    :return: None
+    """
     logging_format = '%(asctime)s %(levelname)9s %(lineno)4s %(module)s: %(message)s'
     logging.basicConfig(level=logging.INFO, format=logging_format)
 
 
 def get_environment_variable(var):
+    """
+    Extracts environment variables
+    :param var:
+    :return: value of the environment variable.
+    """
     try:
         return os.environ[var]
     except KeyError as excep:
@@ -25,13 +37,39 @@ def get_environment_variable(var):
 
 
 def select_random(sample_list, sample_size):
+    """
+    Select a random sample of size sample_size
+    :param sample_list:
+    :param sample_size:
+    :return:
+    """
     sample = random.sample(sample_list, sample_size)
     return sample
 
 
-def run_ballot():
-    meetup_key = get_environment_variable(MEETUP_KEY_VAR)
-    meetup_urlname = get_environment_variable(MEETUP_URLNAME_VAR)
+def check_meetup_is_in_less_than_delta_time(meetup_key, meetup_urlname, days):
+    """
+    Checks if the meetup is less than delta days away. Useful
+    for triggering cron.
+    :param meetup_key: Meetup.com API Key
+    :param meetup_urlname: Meetup's group API.
+    :param days:
+    :return: bool True if meetup is less than delta days away else False
+    """
+    today = datetime.utcnow()
+    time_after_delta = today + timedelta(days=days)
+    client = MeetupClient(key=meetup_key, urlname=meetup_urlname)
+    next_event_time = client.get_next_event_time()/1000
+    return next_event_time < time_after_delta
+
+
+def run_ballot(meetup_key, meetup_urlname):
+    """
+    Run's the PyData London Meetups's RSVP Ballot.
+    :param meetup_key: Meetup.com API Key
+    :param meetup_urlname: Url name of the meetup group.
+    :return: None
+    """
 
     logging.info('Creating Meetup Client')
     client = MeetupClient(key=meetup_key, urlname=meetup_urlname)
@@ -57,6 +95,24 @@ def run_ballot():
     client.mark_rsvps_to_yes(next_event_id, attending_members)
 
 
-if __name__ == '__main__':
+def main():
+    """
+    Main function which will be executed when this scripted is invoked directly.
+    Setups logging, checks if its the right time to run the ballot and runs the
+    ballot if its the right time.
+    :return: None
+    """
     setup_logging()
-    run_ballot()
+    meetup_key = get_environment_variable(MEETUP_KEY_VAR)
+    meetup_urlname = get_environment_variable(MEETUP_URLNAME_VAR)
+    if check_meetup_is_in_less_than_delta_time(meetup_key, meetup_urlname, days=RSVP_BEFORE_DAYS):
+        logging.info('The next meetup is less than %s days ago.', RSVP_BEFORE_DAYS)
+        logging.info('Running the PyData London Meetup"s RSVP Ballot')
+        run_ballot(meetup_key, meetup_urlname)
+    else:
+        logging.info('The next meetup is more than %s days ago.', RSVP_BEFORE_DAYS)
+        logging.info('It"s not the right time to run the ballot.')
+
+
+if __name__ == '__main__':
+    main()
--- meetup_ballot/meetup.py
+++ meetup_ballot/meetup.py
@@ -7,6 +7,9 @@ RSVP_YES = 'yes'
 
 
 class MeetupClient:
+    """
+    Client for accessing Meetup.com API
+    """
 
     def __init__(self, key, urlname=None):
         self.key = key
@@ -16,10 +19,20 @@ class MeetupClient:
         self.setup_logging()
 
     def setup_logging(self):
+        """
+        Setups the logging.
+        :return: None
+        """
         logging_format = '%(asctime)s %(levelname)9s %(lineno)4s %(module)s: %(message)s'
         logging.basicConfig(level=logging.INFO, format=logging_format)
 
     def send_get_request(self, params, append_url):
+        """
+        Send GET request to meetup's API.
+        :param params: dict of params
+        :param append_url: string to append in the url.
+        :return: response of the GET request.
+        """
         url = '{default_url}/{append_url}'.format(default_url=self.url, append_url=append_url)
         default_params = {'key': self.key}
         req_params = {**params, **default_params}
@@ -27,10 +40,28 @@ class MeetupClient:
         return response
 
     def get_next_event_id(self):
+        """
+        Id of the next meetup of the group.
+        :return:
+        """
         response = self.send_get_request({'page': 1}, 'events')
         return response.json()[0]['id']
 
+    def get_next_event_time(self):
+        """
+        Unix Time of next event in milliseconds in UTC.
+        :return: int
+        """
+        response = self.send_get_request({'page': 1}, 'events')
+        return response.json()[0]['time']
+
     def mark_rsvps_to_yes(self, event_id, member_ids):
+        """
+        Marks the RSVP of all the members in the member_ids for a given event_id
+        :param event_id: id of the event
+        :param member_ids: list of member_id
+        :return: None
+        """
         url = '{base_url}/2/rsvp'.format(base_url=self.base_url)
         for member_id in member_ids:
             logging.info('Setting RSVP to yes for member_id: %s', member_id)
@@ -42,11 +73,21 @@ class MeetupClient:
                 logging.info('Marked member_id: %s"s RSVP as Yes', member_id)
 
     def get_rsvps(self, event_id):
+        """
+        Get all the RSVPS for a given event id.
+        :param event_id:
+        :return: json response
+        """
         append_url = 'events/{event_id}/rsvps'.format(event_id=event_id)
         response = self.send_get_request({}, append_url=append_url)
         return response.json()
 
     def get_coorganizers_and_hosts_from_rsvps(self, rsvps):
+        """
+        Get the RSVPS of all the coorganizers of the meetup group.
+        :param rsvps:
+        :return: list of member ids.
+        """
         member_ids = []
         for rsvp in rsvps:
             member = rsvp['member']
@@ -55,6 +96,11 @@ class MeetupClient:
         return member_ids
 
     def get_member_ids_from_rsvps(self, rsvps):
+        """
+        Get member_ids of non-coorganizers and non-hosts from the RSVPS.
+        :param rsvps:
+        :return: list of member ids.
+        """
         member_ids = []
         for rsvp in rsvps:
             member = rsvp['member']
