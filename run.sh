469b8f13e8781469cd4920906088c792adc90407
Prashant Tiwari <prashanttiwari247@gmail.com>
2018-03-04 12:39:27 +0000

    Change dir after launching bash in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+cd /home/prashant/work/pydata/pydata-london-ballot/
 echo "Exporting environment variables from .env"
 export $(cat .env | xargs)
 echo "Starting Ballot Runner"
-PYTHONPATH='.' python meetup_ballot/ballot.py
+PYTHONPATH='.' python3 meetup_ballot/ballot.py
 echo "Execution complete! Check logs in ballot.log & meetup.log"
