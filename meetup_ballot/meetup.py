407647e539f4c8930b86ac98e33fbcd97a302d7b
chfw <wangc_2011@hotmail.com>
2018-11-23 18:24:02 +0000

    :microscope: more unit tests for meetup code

--- meetup_ballot/meetup.py
+++ meetup_ballot/meetup.py
@@ -8,7 +8,8 @@ from collections import Counter
 
 NUM_OF_REQUESTS_TO_SLEEP_AFTER = 2
 
-RSVP_YES = 'yes'
+RSVP_YES = "yes"
+LOG_FORMAT = "%(asctime)s %(levelname)9s %(lineno)4s %(module)s: %(message)s"
 
 
 class MeetupClient:
@@ -19,8 +20,8 @@ class MeetupClient:
     def __init__(self, key, urlname=None):
         self.key = key
         self.urlname = urlname
-        self.base_url = 'https://api.meetup.com'
-        self.url = '{}/{}'.format(self.base_url, self.urlname)
+        self.base_url = "https://api.meetup.com"
+        self.url = "{}/{}".format(self.base_url, self.urlname)
         self.setup_logging()
 
     def setup_logging(self):
@@ -28,9 +29,10 @@ class MeetupClient:
         Setups the logging.
         :return: None
         """
-        logging_file = 'meetup.log'
-        logging_format = '%(asctime)s %(levelname)9s %(lineno)4s %(module)s: %(message)s'
-        logging.basicConfig(level=logging.INFO, format=logging_format, filename=logging_file)
+        logging_file = "meetup.log"
+        logging.basicConfig(
+            level=logging.INFO, format=LOG_FORMAT, filename=logging_file
+        )
 
     def send_get_request(self, params, append_url):
         """
@@ -39,8 +41,10 @@ class MeetupClient:
         :param append_url: string to append in the url.
         :return: response of the GET request.
         """
-        url = '{default_url}/{append_url}'.format(default_url=self.url, append_url=append_url)
-        default_params = {'key': self.key}
+        url = "{default_url}/{append_url}".format(
+            default_url=self.url, append_url=append_url
+        )
+        default_params = {"key": self.key}
         req_params = {**params, **default_params}
         response = requests.get(url, params=req_params)
         return response
@@ -50,11 +54,11 @@ class MeetupClient:
         Id of the next meetup of the group.
         :return:
         """
-        response = self.send_get_request({'page': 1}, 'events')
+        response = self.send_get_request({"page": 1}, "events")
         if response.status_code == 200 and len(response.json()) > 0:
-            return response.json()[0]['id']
+            return response.json()[0]["id"]
         else:
-            logging.error('No future events scheduled')
+            logging.error("No future events scheduled")
             return 0
 
     def get_member_details(self, member_id):
@@ -63,9 +67,12 @@ class MeetupClient:
         :param member_id: meetup.com id of the member
         :return: json of member details
         """
-        url = '{base_url}/2/member/{member_id}'.format(base_url=self.base_url, member_id=member_id)
-        logging.info('Getting member details from url: {}'.format(url))
-        response = requests.get(url=url, params={'key': self.key, 'page': 1})
+        url = "{base_url}/2/member/{member_id}".format(
+            base_url=self.base_url, member_id=member_id
+        )
+
+        logging.info("Getting member details from url: {}".format(url))
+        response = requests.get(url=url, params={"key": self.key, "page": 1})
         return response.json()
 
     def get_member_name(self, member_id):
@@ -75,45 +82,62 @@ class MeetupClient:
         :return: name of the member.
         """
         member_details = self.get_member_details(member_id)
-        return member_details.get('name')
+        return member_details.get("name")
 
     def get_next_event_time(self):
         """
         Unix Time of next event in milliseconds in UTC.
         :return: int
         """
-        response = self.send_get_request({'page': 1}, 'events')
+        response = self.send_get_request({"page": 1}, "events")
         if response.status_code == 200 and len(response.json()) > 0:
-            return response.json()[0]['time']
+            return response.json()[0]["time"]
         else:
-            logging.info('Next event info response: %s', response.status_code)
-            logging.info('Next event info response: %s', response.json())
-            logging.error('Either no future events scheduled or unable to fetch any.')
-            return float('INF')
+            logging.info("Next event info response: %s", response.status_code)
+            logging.info("Next event info response: %s", response.json())
+            logging.error(
+                "Either no future events scheduled or unable to fetch any."
+            )
+            return float("INF")
 
     def mark_rsvps_to_yes(self, event_id, member_ids):
         """
-        Marks the RSVP of all the members in the member_ids for a given event_id
+        Marks the RSVP of all the members in the member_ids
+        for a given event_id
+
         :param event_id: id of the event
         :param member_ids: list of member_id
         :return: None
         """
-        url = '{base_url}/2/rsvp'.format(base_url=self.base_url)
+        url = "{base_url}/2/rsvp".format(base_url=self.base_url)
         total_members = len(member_ids)
         for i in range(total_members):
             if i % NUM_OF_REQUESTS_TO_SLEEP_AFTER == 0:
                 time.sleep(1)
             member_id = member_ids[i]
-            logging.info('%s/%s Setting RSVP to yes for member_id: %s', i + 1, total_members, member_id)
-            data = {'member_id': member_id, 'event_id': event_id, 'rsvp': RSVP_YES, 'key': self.key}
+            logging.info(
+                "%s/%s Setting RSVP to yes for member_id: %s",
+                i + 1,
+                total_members,
+                member_id,
+            )
+            data = {
+                "member_id": member_id,
+                "event_id": event_id,
+                "rsvp": RSVP_YES,
+                "key": self.key,
+            }
             response = requests.post(url, data=data)
             if response.status_code != 201:
                 try:
                     response_json = response.json()
                 except Exception:
-                    response_json = 'no response json'
-                logging.info('Something went wrong! Response code: %s, Response: %s', response.status_code,
-                             response_json)
+                    response_json = "no response json"
+                logging.info(
+                    "Something went wrong! Response code: %s, Response: %s",
+                    response.status_code,
+                    response_json,
+                )
             else:
                 logging.info('Marked member_id: %s"s RSVP as Yes', member_id)
 
@@ -123,7 +147,7 @@ class MeetupClient:
         :param event_id:
         :return: json response
         """
-        append_url = 'events/{event_id}/rsvps'.format(event_id=event_id)
+        append_url = "events/{event_id}/rsvps".format(event_id=event_id)
         response = self.send_get_request({}, append_url=append_url)
         return response.json()
 
@@ -134,7 +158,7 @@ class MeetupClient:
         :return: Counter dict of response:
         e.g. Counter({'no': 42, 'waitlist': 162, 'yes': 254})
         """
-        response = Counter([rsvp['response'] for rsvp in rsvps])
+        response = Counter([rsvp["response"] for rsvp in rsvps])
         return response
 
     def get_coorganizers_and_hosts_from_rsvps(self, rsvps):
@@ -145,10 +169,10 @@ class MeetupClient:
         """
         member_ids = []
         for rsvp in rsvps:
-            member = rsvp['member']
-            if member.get('role') or member['event_context']['host']:
-                member_ids.append(rsvp['member']['id'])
-        logging.info('Co-organizers IDs: %s', member_ids)
+            member = rsvp["member"]
+            if member.get("role") or member["event_context"]["host"]:
+                member_ids.append(rsvp["member"]["id"])
+        logging.info("Co-organizers IDs: %s", member_ids)
         return member_ids
 
     def get_member_ids_from_rsvps(self, rsvps):
@@ -159,7 +183,7 @@ class MeetupClient:
         """
         member_ids = []
         for rsvp in rsvps:
-            member = rsvp['member']
-            if not member.get('role') and not member['event_context']['host']:
-                member_ids.append(rsvp['member']['id'])
+            member = rsvp["member"]
+            if not member.get("role") and not member["event_context"]["host"]:
+                member_ids.append(rsvp["member"]["id"])
         return member_ids
--- tests/test_meetup.py
+++ tests/test_meetup.py
@@ -1,31 +1,136 @@
 # coding=utf-8
 
-import unittest
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
 from meetup_ballot.meetup import MeetupClient
 
+DEFAULT_URL_NAME = "pydatalondon"
+DEFAULT_KEY = "abcd"
 
-class TestMeetup(unittest.TestCase):
 
-    @staticmethod
-    def get_rsvp(id, role=False, host=False):
-        rsvp = {
-            'member': {
-                'id': id,
-                'event_context': {'host': host}
-            },
+class TestMeetup(TestCase):
+    def setUp(self):
+        self.client = MeetupClient(key=DEFAULT_KEY, urlname=DEFAULT_URL_NAME)
+
+    @patch("requests.get")
+    def test_send_get_request(self, mocked_get):
+        append_url = "rsvps"
+        test_params = dict(hello="world")
+        self.client.send_get_request(test_params, append_url)
+
+        expected_url = "https://api.meetup.com/{url_name}/{append_url}".format(
+            url_name=DEFAULT_URL_NAME, append_url=append_url
+        )
+        expected_params = dict(key=DEFAULT_KEY)
+        expected_params.update(test_params)
+        mocked_get.assert_called_with(expected_url, params=expected_params)
+
+    @patch("requests.get")
+    def test_get_next_event_id(self, mocked_get):
+        fake_id = "123"
+        fake_response = [dict(id=fake_id)]
+        mocked_get.return_value = MagicMock(
+            status_code=200, json=MagicMock(return_value=fake_response)
+        )
+        actual = self.client.get_next_event_id()
+
+        self.assertEqual(fake_id, actual)
+
+    @patch("requests.get")
+    def test_get_member_details(self, mocked_get):
+        fake_member_details = dict(name="testbot")
+        mocked_get.return_value = MagicMock(
+            json=MagicMock(return_value=fake_member_details)
+        )
+        self.client.get_member_details("abc")
+
+        expected_url = "https://api.meetup.com/2/member/abc"
+        expected_params = {"key": "abcd", "page": 1}
+
+        mocked_get.assert_called_with(url=expected_url, params=expected_params)
+
+    @patch("requests.get")
+    def test_get_member_name(self, mocked_get):
+        test_name = "testbot"
+        fake_member_details = dict(name=test_name)
+        mocked_get.return_value = MagicMock(
+            json=MagicMock(return_value=fake_member_details)
+        )
+        member_name = self.client.get_member_name("abc")
+        self.assertEqual(test_name, member_name)
+
+    @patch("requests.get")
+    def test_get_next_event_time(self, mocked_get):
+        fake_time = "4 December"
+        fake_response = [dict(time=fake_time)]
+        mocked_get.return_value = MagicMock(
+            status_code=200, json=MagicMock(return_value=fake_response)
+        )
+
+        next_date = self.client.get_next_event_time()
+        self.assertEqual(fake_time, next_date)
+
+    @patch("time.sleep")
+    @patch("requests.post")
+    def test_mark_rsvps_to_yes(self, mocked_post, _):
+        fake_event_id = "event_id"
+        fake_member_ids = ["p1", "p2"]
+        mocked_post.return_value = MagicMock(
+            status_code=200, json=MagicMock(return_value="OK")
+        )
+        self.client.mark_rsvps_to_yes(fake_event_id, fake_member_ids)
+
+        expected_url = "https://api.meetup.com/2/rsvp"
+        expected_data = {
+            "member_id": "p2",
+            "event_id": "event_id",
+            "rsvp": "yes",
+            "key": "abcd",
         }
-        if role:
-            rsvp['member']['role'] = 'coorganizer'
-        return rsvp
+        mocked_post.assert_called_with(expected_url, data=expected_data)
+
+    @patch("requests.get")
+    def test_get_rsvps(self, mocked_get):
+        fake_event_id = "event_id"
+        mocked_get.return_value = MagicMock(
+            status_code=200, json=MagicMock(return_value="OK")
+        )
+        self.client.get_rsvps(fake_event_id)
+
+        expected_url = (
+            "https://api.meetup.com/" + "pydatalondon/events/event_id/rsvps"
+        )
+        mocked_get.assert_called_with(expected_url, params={"key": "abcd"})
+
+    def test_get_response_wise_rsvps_count(self):
+        fake_rsvps = [{"response": "yes"}, {"response": "no"}]
+        count = self.client.get_response_wise_rsvps_count(fake_rsvps)
+
+        self.assertEqual({"yes": 1, "no": 1}, count)
+
+    def test_get_coorganizers_and_hosts_from_rsvps(self):
+        fake_rsvps = [
+            get_rsvp(1, role=True, host=True)
+        ]
+        response = self.client.get_coorganizers_and_hosts_from_rsvps(
+            fake_rsvps
+        )
+        self.assertEqual([1], response)
 
     def test_get_member_ids_from_rsvps(self):
         rsvps = [
-            self.get_rsvp(id=1, role=False, host=False),
-            self.get_rsvp(id=2, role=True, host=False),
-            self.get_rsvp(id=3, role=False, host=True),
-            self.get_rsvp(id=4, role=False, host=False),
-            self.get_rsvp(id=5, role=True, host=True)
+            get_rsvp(id=1, role=False, host=False),
+            get_rsvp(id=2, role=True, host=False),
+            get_rsvp(id=3, role=False, host=True),
+            get_rsvp(id=4, role=False, host=False),
+            get_rsvp(id=5, role=True, host=True),
         ]
-        client = MeetupClient(key='abcd')
-        member_ids = client.get_member_ids_from_rsvps(rsvps=rsvps)
+        member_ids = self.client.get_member_ids_from_rsvps(rsvps=rsvps)
         self.assertListEqual([1, 4], member_ids)
+
+
+def get_rsvp(id, role=False, host=False):
+    rsvp = {"member": {"id": id, "event_context": {"host": host}}}
+    if role:
+        rsvp["member"]["role"] = "coorganizer"
+    return rsvp
